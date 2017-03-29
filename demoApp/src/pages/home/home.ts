895acc082c59d5933300869509bb22ebdd80458c
qiushenyou <88649086@qq.com>
2017-03-29 18:09:37 +0800

    okok

--- demoApp/src/pages/home/home.ts
+++ demoApp/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component,ViewChild,ElementRef } from '@angular/core';
-import { NavController,Slides,Content,Grid } from 'ionic-angular';
+import { NavController,Slides,Content } from 'ionic-angular';
 
 @Component({
     selector: 'page-home',
