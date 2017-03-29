6643613b85e934308faa92eb4a843ac1e8b1d6ed
qiushenyou <88649086@qq.com>
2017-03-29 18:00:33 +0800

    ok

--- demoApp/src/pages/home/home.ts
+++ demoApp/src/pages/home/home.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { Component,ViewChild,ElementRef } from '@angular/core';
+import { NavController,Slides,Content,Grid } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    @ViewChild(Slides) slides: Slides;
+    @ViewChild(Content) content: Content;
+    @ViewChild('searchGrid') searchGrid: ElementRef;
+    constructor(public navCtrl: NavController) {
 
-  constructor(public navCtrl: NavController) {
+    }
 
-  }
+    ionViewDidEnter() {
+        this.slides.startAutoplay();
+        this.content.enableScrollListener();
+    }
+
+    onPageScroll($event){
+        let top = $event.scrollTop;
+        if(top>=150){
+            this.searchGrid.nativeElement.style.background = "rgba(255,101,30,1)";
+        }else{
+            this.searchGrid.nativeElement.style.background = "linear-gradient(to top, rgba(0,0,0,0), rgba(0,0,0,0.4))";
+        }
+    }
 
 }
